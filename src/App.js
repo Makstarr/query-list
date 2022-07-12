1bf13b1f3bf807e196369a486d89c3c359ccef90
makstarr <maksi112@icloud.com>
2022-07-12 23:22:04 +0300

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import queryString from "query-string";
+
+import "./App.css";
+import { AddTagForm } from "./components/add-tag-form";
+import { TagsList } from "./components/tags-list";
+import { useState } from "react";
 
 function App() {
+  const { tags } = queryString.parse(window.location.search);
+  const [tagsList, setTagsList] = useState(tags && tags.split(","));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddTagForm setTagsList={setTagsList} tagsList={tagsList} />
+      {tagsList && <TagsList tagsList={tagsList} setTagsList={setTagsList} />}
     </div>
   );
 }
