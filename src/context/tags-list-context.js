744e378d6ab408e5081d0675b2007ecb5f46b0d4
makstarr <maksi112@icloud.com>
2022-07-13 00:14:20 +0300

    Context file

---
+++ src/context/tags-list-context.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+import queryString from "query-string";
+
+export const TagsListContext = createContext({ tagsList: [] });
+
+export const TagsListContextProvider = ({ children }) => {
+  const { tags } = queryString.parse(window.location.search);
+  const [tagsList, setTagsList] = useState(tags ? tags.split(",") : []);
+
+  return (
+    <TagsListContext.Provider
+      value={{
+        tagsList,
+        setTagsList,
+      }}
+    >
+      {children}
+    </TagsListContext.Provider>
+  );
+};
