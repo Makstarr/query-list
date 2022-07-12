308987876a339bef50db8d23a54455d2ef90ecf6
makstarr <maksi112@icloud.com>
2022-07-12 23:52:01 +0300

    Empty tag list rendering fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <AddTagForm setTagsList={setTagsList} tagsList={tagsList} />
-      {tagsList.length && <TagsList tagsList={tagsList} setTagsList={setTagsList} />}
+      {tagsList.length ? <TagsList tagsList={tagsList} setTagsList={setTagsList} /> : null}
     </div>
   );
 }
